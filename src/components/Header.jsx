ed4967d0d5807dd5b86b1ce98e42158f6a72af78
Karin Melo <karinsantosmelo@gmail.com>
2022-08-23 22:56:07 -0300

    Requisitos 4 e 5 ok e 6 em andamento

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -25,6 +26,9 @@ render() {
       <div data-testid="header-user-name">
         { IsLoading ? <Loading /> : Login }
       </div>
+      <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+      <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+      <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
     </div>
   );
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,85 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 export default class Search extends Component {
-  render() {
-    return (
-      <div data-testid="page-search">
-        Search
-        <Header />
-      </div>
-    );
+state={
+  search: '',
+  artist: '',
+  loading: false,
+  albuns: [],
+};
+
+  handleInput = ({ target }) => {
+    this.setState({ search: target.value });
   }
+
+    handleClick = async (e) => {
+      const { search } = this.state;
+      e.preventDefault();
+      this.setState({ loading: true });
+      const albuns = await searchAlbumsAPI(search);
+      this.setState({ loading: false, albuns, artist: search });
+    };
+
+    render() {
+      const {
+        artist,
+        search,
+        loading,
+        albuns,
+      } = this.state;
+      const tamMin = 2;
+      return (
+        <div data-testid="page-search">
+          Search
+          <Header />
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.handleInput }
+            />
+
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ search.length < tamMin }
+              onClick={ this.handleClick }
+            >
+              Pesquisar
+            </button>
+          </form>
+          { artist.length > 0 && (
+            <p>
+              {`Resultado de álbuns de: ${artist}`}
+            </p>)}
+          { artist.length > 0 && !albuns.length && (
+            <p>Nenhum álbum foi encontrado</p>)}
+          {loading ? <Loading /> : (
+            albuns.map(({ artistName, collectionId, collectionName, artworkUrl100 }) => (
+              <Link
+                data-testid={ `link-to-album-${collectionId}` }
+                key={ collectionId }
+                to={ `/album/${collectionId}` }
+              >
+                <img src={ artworkUrl100 } alt={ collectionName } />
+                <h3>
+                  {
+                    collectionName
+                  }
+                </h3>
+                <p>
+                  {
+                    artistName
+                  }
+                </p>
+              </Link>
+            ))
+          )}
+        </div>
+      );
+    }
 }
